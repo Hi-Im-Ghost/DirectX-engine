6590d63c3cceafa6968b2a69cfb9ae781aac8372
Hi-Im-Ghost <adikx107@gmail.com>
2022-06-04 16:14:26 +0200

    Revert "Merge branch 'main' of https://github.com/Hi-Im-Ghost/DirectX-engine"
    This reverts commit e2ac4508d8f6ecaa2d86427bf859304d3c9b04b4, reversing
    changes made to 8594d54a3ab84a06f0ce24bcedbd1dfda9b0596e.

--- DirectX-Engine/App.h
+++ DirectX-Engine/App.h
@@ -15,8 +15,6 @@ private:
 private:
 	Window win;
 	Timer timer;
-	std::vector<std::unique_ptr<class Drawable>> drawables;
-	//Liczba obiektów
-	static constexpr size_t nDrawables = 180;
+	std::vector<std::unique_ptr<class Box>> boxes;
 };
 
